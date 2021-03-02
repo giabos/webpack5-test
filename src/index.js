6544e66e832fe76fbb1d96659dbaff76c0413f4b
Gianni <gb@idefix.mydns>
2021-03-02 23:38:42 +0100

    added loader

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import { html, virtual, component, useState } from 'haunted';
 import styles from './index.scss';
 
-const list = n => Array(n).fill().map((_,i)=>i+1);
-const List = virtual((n) => html`<ul>${ list(n).map(a => html`<li>${a}</li>`)}</ul>`);
 
-function App () {
+function App ({loading}) {
         const [count, setCount] = useState(1);
     return html`
         <style>${styles}</style>
-        <h1>test</h1>
-        <button @click=${()=>setCount(count+1)}>click</button>
-        ${List(count)}
+
+        <div><slot></slot></div>
+        <div class="loading" style="display: ${loading ? "block" : "none"}"></div>
+        
+        
     `; 
 }
 
-customElements.define('custom-root', component(App));
+customElements.define('custom-root', component(App, { observedAttributes: ['loading'] }));
     
